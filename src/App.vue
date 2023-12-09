51b0135a5637df36899c91671cc410d6b8ab28a7
MargaritaBusyginaCa <busygina.rita.ca@gmail.com>
2023-12-09 18:57:36 -0500

    configure tailwind.css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 class="bg-amber-300">Hello Vue 3 + Vite</h1>
+  <div class="justify-center flex bg-blue-300 items-center h-screen">
+    <div class="text-4xl">Hello Welcome to Tailwind Css</div>
+  </div>
 </template>
 
 <script>
