b87df6b02b96d90ed86b6d7c937269ceb11ffb02
MargaritaBusyginaCa <busygina.rita.ca@gmail.com>
2024-01-23 16:13:57 -0500

    add interactive footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,17 +2,12 @@
 import backButton from "../assets/icons/icon-back-button.svg";
 import nextButton from "../assets/icons/icon-next-button.svg";
 import content from "../assets/content.json";
-import { useRouter } from "vue-router";
-import { ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { ref, computed } from "vue";
 
+const route = useRoute();
 const router = useRouter();
-const props = defineProps({
-  paintingIndex: {
-    type: Number,
-    default: 0,
-  },
-});
-const currentIndex = ref(props.paintingIndex);
+const currentIndex = ref(route.params.id);
 const emit = defineEmits(["nextPainting", "previousPainting"]);
 function nextPainting() {
   currentIndex.value++;
@@ -30,21 +25,48 @@ const navigate = () => {
     params: { id: currentIndex.value },
   });
 };
+const activeTab = computed(() => {
+  const totalPaintings = content.length;
+  let currentPainting = currentIndex.value;
+  const finalPercentage =
+    Math.round((++currentPainting / totalPaintings) * 10000) / 100;
+
+  return `${finalPercentage}%`;
+});
 </script>
 <template>
-  <div class="flex justify-between items-center">
-    <div>
-      <p>{{ content[paintingIndex].name }}</p>
-      <p>{{ content[paintingIndex].artist.name }}</p>
+  <section>
+    <div class="stepper">
+      <div class="active-tab"></div>
     </div>
-    <div class="flex gap-[40px]">
-      <button @click="previousPainting">
-        <img :src="backButton" alt="go-back-button" />
-      </button>
+    <div class="flex justify-between items-center">
+      <div>
+        <p>{{ content[currentIndex].name }}</p>
+        <p>{{ content[currentIndex].artist.name }}</p>
+      </div>
+      <div class="flex gap-[40px]">
+        <button @click="previousPainting">
+          <img :src="backButton" alt="go-back-button" />
+        </button>
 
-      <button @click="nextPainting">
-        <img :src="nextButton" alt="go-next-button" />
-      </button>
+        <button @click="nextPainting">
+          <img :src="nextButton" alt="go-next-button" />
+        </button>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+.stepper {
+  width: 100%;
+  height: 2px;
+  background-color: $light_grey;
+  margin-bottom: 24px;
+  .active-tab {
+    width: v-bind(activeTab);
+    height: 2px;
+    background-color: $black;
+  }
+}
+</style>
