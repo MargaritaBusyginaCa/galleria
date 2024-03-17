e9fdeed71969dcfeffd9464b09fbeb57f7e9f4ef
MargaritaBusyginaCa <busygina.rita.ca@gmail.com>
2024-03-17 16:27:22 -0400

    style navbar

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,10 @@ import Navbar from "@/components/Navbar.vue";
 @import "./assets/scss/variables.scss";
 @import "./assets/scss/theme.scss";
 .app {
-  padding: 40px 40px 0 40px;
   width: 100%;
   height: 100vh;
+}
+main {
+  padding: 20px;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,34 @@
 <script setup>
 import logo from "@/assets/icons/logo.svg";
+const startSlideText = "Start slideshow";
 </script>
 
 <template>
   <div class="navbar">
     <router-link to="/"> <img :src="logo" alt="galleria logo" /></router-link>
+    <router-link to="/">
+      <p>{{ startSlideText }}</p>
+    </router-link>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import "../assets/scss/variables.scss";
 .navbar {
-  padding-bottom: 20px;
+  //   padding-bottom: 20px;
   border-bottom: 1px solid $alt_grey;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0;
+  margin: 20px;
+  a {
+    text-decoration: none;
+  }
+  p {
+    text-transform: uppercase;
+    font-size: 12px;
+    letter-spacing: 2.57px;
+  }
 }
 </style>
