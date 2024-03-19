901167f314f29ac359e2f51ad970f1249ef07d34
MargaritaBusyginaCa <busygina.rita.ca@gmail.com>
2024-03-19 19:31:14 -0400

    fix reload bug

--- src/pages/Slideshow.vue
+++ src/pages/Slideshow.vue
@@ -2,10 +2,15 @@
 import { usePaintingStore } from "@/stores/painting";
 import PaintingMain from "@/components/PaintingMain.vue";
 import PaintingHistory from "@/components/PaintingHistory.vue";
-// import content from "@/assets/content.json";
 import Footer from "@/components/Footer.vue";
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
 
+const route = useRoute();
 const paintingStore = usePaintingStore();
+onMounted(() => {
+  paintingStore.setIndex(route.params.id);
+});
 </script>
 <template>
   <section class="painting-information">
